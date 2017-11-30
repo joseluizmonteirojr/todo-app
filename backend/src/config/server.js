43ef52aa29bd72f50ef4fa996050cd0c67f0bc77
zezinho_monteirojr <zezinho_monteirojr@hotmail.com>
2017-11-29 23:26:38 -0200

    Seção 4 - Aula 33 - Configurando o servidor com o Express

---
+++ backend/src/config/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const port = 3003;
+const server = express();
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+server.listen(port, ()=>{
+    console.log(`****BACKEND is running on port: ${port}.****`)
+})
