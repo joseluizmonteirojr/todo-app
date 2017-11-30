f2bac041397b69aeda3e0c8f2f2f1ea9828005dd
zezinho_monteirojr <zezinho_monteirojr@hotmail.com>
2017-11-29 23:55:17 -0200

    Seção 4 - Aula 34 - Conexão com o Banco de Dados

---
+++ backend/src/config/database.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise; //garantindo que a api promise do mongoose vai usar a do node
+
+module.exports = mongoose.connect('mongodb://localhost/todo');
