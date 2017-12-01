ec9528d82b11f61724d4ba81d0d1d33c55b6b724
zezinho_monteirojr <zezinho_monteirojr@hotmail.com>
2017-11-30 23:20:46 -0200

    Seção 4/5 - Aulas 38 a 40 - Habilitando o CORS, Testando backend com PM2 e Configurando parte frontend

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const port = 3003;
 const server = express();
+const allowsCors = require('./cors');
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(allowsCors);
 
-server.listen(port, ()=>{
+server.listen(port, () => {
     console.log(`****BACKEND is running on port: ${port}.****`)
 });
 
